ecb6b97992b775ee1a09eb2f70078eeaf65eb76d
lennchi <elena.haramulova@gmail.com>
2024-05-19 13:20:45 +0200

    random apple variety picker

--- varieties.py
+++ varieties.py
@@ -10,8 +10,8 @@ varieties = df.to_list()
 varieties = [variety.strip('\xa0') for variety in varieties]
 
 # Create a random variety picker
-st.title('Jablko dne')
+st.title('🍎 Jablko dne 🍎')
 
-if st.button('Vylosuj si 🍎'):
+if st.button('Vylosuj si odrůdu'):
     your_apple = random.choice(varieties)
     st.write(your_apple)
