8e1b46635267cbb2e4dcc85090e132e0607c2e1f
lennchi <elena.haramulova@gmail.com>
2024-05-25 22:11:19 +0200

    fruit slot machine

--- slot_machine.py
+++ slot_machine.py
@@ -2,35 +2,36 @@ import streamlit as st
 from PIL import Image
 
 
-def get_fruits(year):
+# def get_fruits(year):
+#     """Identify user's generation based on their birth year and get fruit images for them"""
+#     # Boomers
+#     if year < 1964:
+#         return [img_apple, img_strawberry, img_banana], ['37 %', '36 %', '34 %']
+#     # Gen X
+#     elif 1964 <= year < 1981:
+#         return [img_banana, img_strawberry, img_apple], ['42 %', '39 %', '30 %']
+#     # Millenials
+#     elif 1981 <= year < 1997:
+#         return [img_banana, img_strawberry, img_apple], ['42 %', '39 %', '34 %']
+#     # Gen Z
+#     else:
+#         return [img_strawberry, img_banana, img_apple], ['45 %', '34 %', '23 %']
+
+
+def get_fruits(generation):
     """Identify user's generation based on their birth year and get fruit images for them"""
     # Boomers
-    if year < 1964:
+    if generation == 'Baby boomer (1940–1963)':
         return [img_apple, img_strawberry, img_banana], ['37 %', '36 %', '34 %']
     # Gen X
-    elif 1964 <= year < 1981:
+    elif generation == 'Gen X (1964–1980)':
         return [img_banana, img_strawberry, img_apple], ['42 %', '39 %', '30 %']
     # Millenials
-    elif 1981 <= year < 1997:
+    elif generation == 'Mileniál (1981–1996)':
         return [img_banana, img_strawberry, img_apple], ['42 %', '39 %', '34 %']
     # Gen Z
     else:
         return [img_strawberry, img_banana, img_apple], ['45 %', '34 %', '23 %']
-
-def get_generation(year):
-    """Identify user's generation based on their birth year"""
-    # Boomers
-    if year < 1964:
-        return 'Boomer'
-    # Gen X
-    elif 1964 <= year < 1981:
-        return 'GenX'
-    # Millenials
-    elif 1981 <= year < 1997:
-        return 'Millenial'
-    # Gen Z
-    else:
-        return 'GenZ'
 
 
 img_apple = "fruit_icons_apple.jpg"
@@ -44,16 +45,16 @@ Víš, jaké jsou TOP-3 ovoce pro tvou generaci?
          """)
 
 # User input for birth year
-birth_year = st.number_input("Zadej rok narození: ", min_value=1940, max_value=2012, step=1)
-
-
+# birth_year = st.number_input("Zadej rok narození: ", min_value=1940, max_value=2012, step=1)
+generation = st.selectbox("", options=['Baby boomer (1940–1963)', 'Gen X (1964–1980)', 'Mileniál (1981–1996)', 'Gen Z (1997–2012)'])
 
 # Slot machine button
 if st.button("Dáme ovoce!"):
-    images = get_fruits(birth_year)[0]
-    captions = get_fruits(birth_year)[1]
-    # (r"$\textsf{\Large Enter text here}$")
+    images = get_fruits(generation)[0]
+    captions = get_fruits(generation)[1]
     slot_1, slot_2, slot_3 = st.columns(3)
+
+
 
 
     with slot_1:
