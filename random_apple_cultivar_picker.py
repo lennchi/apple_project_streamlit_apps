0c8d0e552658ef0f5defff8c7e46791d3a0001c5
lennchi <elena.haramulova@gmail.com>
2024-06-09 20:43:34 +0200

    added extra space

--- random_apple_cultivar_picker.py
+++ random_apple_cultivar_picker.py
@@ -49,7 +49,7 @@ st.image('img/streamlit_apple_900.png')
 
 # Center the header
 header = 'Jablko pro tebe'
-st.markdown(f'<div class="centered"><h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{header}</h1></div>', unsafe_allow_html=True)
+st.markdown(f'<div class="centered"><h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{header}</h1></div>', unsafe_allow_html=True)
 
 # Center the button
 st.markdown('<div class="centered">', unsafe_allow_html=True)
